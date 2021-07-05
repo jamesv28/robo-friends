ec5940e832e0f8bc60ae743199ae0ebca8c62b28
James Volmert <james.volmert@gmail.com>
2021-07-05 16:45:26 -0600

    added redux middleware

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import {searchRobots} from './reducers/reducers';
+import {createLogger} from 'redux-logger';
 import './index.css';
 import App from './App';
 import 'tachyons';
 import reportWebVitals from './reportWebVitals';
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 ReactDOM.render(
   <Provider store={store}>
@@ -17,7 +19,5 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
