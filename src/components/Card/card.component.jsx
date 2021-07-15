2b25a7d5c1b4e82ca5e6d9aea642a0503dd67974
James Volmert <james.volmert@gmail.com>
2021-07-15 17:52:33 -0600

    tried to optimize performance

--- src/components/Card/card.component.jsx
+++ src/components/Card/card.component.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Card = ({username, name, email}) => (
     <div className="bg-light-green dib pa3 ma2 grow bw2 shadow-5 tc" >
-        <img src={`https://robohash.org/${username}`} alt={name}/>
+        <img src={`https://robohash.org/${username}?size=200x200`} alt={name}/>
         <div>
             <h2>{name}</h2>
             <p>{email}</p>
