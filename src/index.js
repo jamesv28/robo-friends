f87b2d64c60a2abd1f6c48fc12cdfe4087f3889f
James Volmert <james.volmert@gmail.com>
2021-07-19 19:31:56 -0600

    finally got the service worker to work

--- src/index.js
+++ src/index.js
@@ -22,6 +22,9 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-serviceWorkerRegistration.unregister();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 reportWebVitals();
