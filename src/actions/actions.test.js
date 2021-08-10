768dafea59f4214210f248c1bb4f1d92099b9303
James Volmert <james.volmert@gmail.com>
2021-08-10 16:51:41 -0600

    added unit test for all of the actions

--- src/actions/actions.test.js
+++ src/actions/actions.test.js
@@ -5,6 +5,10 @@ import {
     REQUEST_ROBOTS_SUCCESS,
     REQUEST_ROBOTS_FAILED
 } from '../constants/constants';
+import configureMockStore from 'redux-mock-store';
+import thunkMiddleware from 'redux-thunk';
+
+const mockStore = configureMockStore([thunkMiddleware]);
 
 it('should create an action to search robots', () => {
     const text = 'woo';
@@ -13,4 +17,15 @@ it('should create an action to search robots', () => {
         payload: text
     }
     expect(actions.setSearchField(text)).toEqual(expectAction);
+});
+
+it('handles requesting api call', () => {
+    const store = mockStore();
+    store.dispatch(actions.requestRobots());
+    const action = store.getActions();
+    const expectAction = {
+        type: REQUEST_ROBOTS_PENDING
+    }
+    expect(action[0]).toEqual(expectAction);
+
 })
